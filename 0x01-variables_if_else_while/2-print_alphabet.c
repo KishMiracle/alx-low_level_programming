f4c81e5b494457fdb803a2b7b5da79ebce299dad
KishMiracle <miracle@gmail.com>
2022-12-09 16:00:16 -0800

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - print a to z lowercase number
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	while(ch <= 'z')
+	{
+		printf("%c ", ch);
+		ch++;
+	}
+	return (0);
+}
