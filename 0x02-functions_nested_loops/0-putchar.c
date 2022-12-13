9c5bd189b356eaf0a706756d636368e94776fe3d
KishMiracle <miracle@gmail.com>
2022-12-13 13:33:58 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_str - Prints a string
+* @str: the string to print
+* Return: void.
+*
+*/
+void print_str(char *str)
+{
+	char tmp;
+	int i = 0;
+
+	tmp = str[0];
+	while (tmp != '\0')
+	{
+		_putchar(tmp);
+		tmp = str[++i];
+	}
+}
+/**
+* main - The entry point of the program.
+* Return: 1 on success. An errno value othewise.
+*/
+int main(void)
+{
+	print_str("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Create _putchar.c file content
+create main.h file withe contentt
