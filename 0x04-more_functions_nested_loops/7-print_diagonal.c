ed8609d1feb0ff9d56d21370a62b07ef354a74e6
KishMiracle <miracle@gmail.com>
2022-12-15 15:05:21 -0800

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,11 +11,14 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	for (i = 0; i < n; i++)
+	else
 	{
-		for (j = 0; j <= i; j++)
-			_putchar(' ');
-		_putchar(92);
-		_putchar('\n');
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 }
