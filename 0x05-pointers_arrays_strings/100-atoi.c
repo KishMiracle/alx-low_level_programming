540adb0b2330a2cbfbe47a80570a85d97632c428
KishMiracle <miracle@gmail.com>
2022-12-19 15:31:14 -0800

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,149 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* _pow - Computes the power of a number by another.
-* @a: The first number.
-* @n: The power to compute.
-* Return: a ^ n.
+* _atoi - Converts a string to an integer.
+* @s: The string to be converted.
+* Return: The integer value of the converted string.
 */
 
-int _pow(int a, int n)
+int _atoi(char *s)
 {
-	int i, res = a;
-
-	if (n == 0)
-		res = 1;
-	else if (n > 1)
-		for (i = 1; i < n; i++)
-			res = res * a;
-	return (res);
-}
-
-/**
-* _isdigit - Checks for digit character.
-* @c: The charactere to check.
-* Return: 1 if c is a digit.
-* 0 otherwise
-*/
-
-int _isdigit(int c)
-{
-	int r = 0;
-
-	if ((c > 47) && (c < 58))
-		r = 1;
-	return (r);
-}
-
-/**
-* char_to_int - Converts a char digit to its integer value.
-* @c: The ascii character.
-* Return: The integer value of c.
-*/
-
-int char_to_int(char c)
-{
-	int res;
-
-	switch ((int)c)
-	{
-	case 48:
-		res =  0;
-		break;
-	case 49:
-		res =  1;
-		break;
-	case 51:
-		res =  3;
-		break;
-	case 52:
-		res =  4;
-		break;
-	case 53:
-		res =  5;
-		break;
-	case 54:
-		res =  6;
-		break;
-	case 55:
-		res =  7;
-		break;
-	case 56:
-		res =  8;
-		break;
-	case 57:
-		res =  9;
-		break;
-	default:
-		res = 0;
-	}
-	return (res);
-}
-
-/**
-* order - Returns the order of an integer contained in a string.
-* @s: The string whose the order should be returned.
-* Return: The order of s.
-*/
-
-int order(char *s)
-{
-	int i = -1, o = 0, found_1st_int = 0;
-	char tmp;
+	int sign = 1;
+	unsigned int num = 0;
 
 	do {
-		tmp = s[++i];
-		if (_isdigit(tmp))
-		{
-			o++;
-			found_1st_int = 1;
-		}
-		else if (found_1st_int)
-		{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
 			break;
-		}
-	} while (tmp != '\0');
-	return (o);
-}
-
-/**
-* _atoi - Conversts a string to an integer.
-* @s: The string to convert
-* Return: The integer value of the string.
-*/
-
-char _atoi(char *s)
-{
-	int ci, i = -1, n = 0, sign = 1, res = 0, found_1st_int = 0;
-	char tmp;
-
-	n = order(s);
-	do {
-		tmp = s[++i];
-		if (tmp == '-')
-		{
-			sign = sign * -1;
-			found_1st_int = 1;
-		}
-		else if (found_1st_int)
-		{
-			break;
-		}
-	} while (tmp != '\0');
-	found_1st_int = 0;
-	i = -1;
-	do {
-		tmp = s[++i];
-		if (_isdigit(tmp))
-		{
-			ci = char_to_int(tmp);
-			res += (sign * ci * _pow(10, --n));
-			found_1st_int = 1;
-		}
-		else if (found_1st_int)
-		{
-			break;
-		}
-	} while (tmp != '\0');
-	return (res);
+	} while (*s++);
+	return (num * sign);
 }
