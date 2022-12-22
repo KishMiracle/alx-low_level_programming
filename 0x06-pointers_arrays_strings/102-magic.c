26e9e69dd6f4f5349598ddc72f9768f293aaae7e
KishMiracle <miracle@gmail.com>
2022-12-22 09:35:12 -0800

    102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 /**
+ * main - check the code
+ * _stdio.h standard library
+ * Return: Always 0.
  * fix error
  */
 
@@ -12,6 +15,14 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+
+	/**
+	 * write your line of code here...
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
 
 	*(p + 5) = 98;
 	printf("a[2] = %d\n", a[2]);
