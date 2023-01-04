074dbe03408ce7bb34138972e88fed1792d4bbef
KishMiracle <miraclekish@gmail.com>
2023-01-04 08:29:46 -0800

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _my_sqrt - returns the natural square root of a number
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
+ * Return: If the number has a natural square root - the square root.
+ * If the number does not have a natural square root - -1.
+ */
+
+int _my_sqrt(int num, int root)
+{
+	if (root * root > num)
+		return (-1);
+	if (root * root == num)
+		return (root);
+	return (_my_sqrt(num, ++root));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * Return: square root of n else -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_my_sqrt(n, 1));
+}
