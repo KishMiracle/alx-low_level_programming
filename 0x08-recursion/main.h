41ad308fd8cc36c1bb57d9b4ddb30910bfb1c7dc
KishMiracle <miraclekish@gmail.com>
2023-01-04 16:17:14 -0800

    101

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -17,5 +17,8 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int find_strlen(char *s);
 int check_palindrome(char *s, int len, int index);
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
 
 #endif
