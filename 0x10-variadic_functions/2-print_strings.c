1b2ce72ebb097b25154dafdb2d57940f046f5883
KishMiracle <miraclekish@gmail.com>
2023-01-19 17:56:51 -0800

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
-		
+
 		if (separator != NULL && i > 0)
 			printf("%s", separator);
 		if (str == NULL)
