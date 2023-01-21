5bbfd9aae1fdd4d4bdc240faf86ececd74b01ee0
KishMiracle <miraclekish@gmail.com>
2023-01-20 20:56:07 -0800

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,44 +3,99 @@
 #include <string.h>
 
 /**
- * count_digit - count number of digits in an integer
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluatedi
  * @i: number to count
  * Return: mumber of digits in the integer
  */
 
-unsigned int count_digit(unsigned int i)
+int is_digit(char *s)
 {
-	unsigned int count = 0;
+	int i = 0;
 
-	do {
-		i /= 10;
-		++count;
-	} while (i != 0);
-	return (count);
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 /**
- * main - multiplies 2 numbers
- * @argc: int argument
- * @argv: pointer to array of string arguments
- * Return: Always 0 (Success)
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	unsigned int num1, num2;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc == 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		if ((strlen(argv[1]) == count_digit(num1))
-				&& (strlen(argv[2]) == count_digit(num2)))
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			printf("%d\n", num1 * num2);
-			return (0);
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	printf("Error\n");
-	exit(98);
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			putchar(result[i] + '0');
+	}
+	if (!a)
+		putchar('0');
+	putchar('\n');
+	free(result);
+	return (0);
 }
